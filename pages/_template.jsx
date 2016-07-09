5f10b2f768748f3f073b77c92d74791ba964ff47
Cole Townsend <cole@coletownsend.com>
2016-07-09 16:11:10 -0400

    init

---
+++ pages/_template.jsx
@@ -0,0 +1,127 @@
+import React from 'react'
+import { Link } from 'react-router'
+import { Container, Grid, Span } from 'react-responsive-grid'
+import { prefixLink } from 'gatsby-helpers'
+import includes from 'underscore.string/include'
+import { colors, activeColors } from 'utils/colors'
+
+import typography from 'utils/typography'
+import { config } from 'config'
+
+// Import styles.
+import 'css/main.css'
+import 'css/github.css'
+
+const { rhythm, fontSizeToPx } = typography
+
+module.exports = React.createClass({
+  propTypes () {
+    return {
+      children: React.PropTypes.object,
+    }
+  },
+  render () {
+    const guideActive = includes(this.props.location.pathname, '/guide/')
+    const resourcesActive = includes(this.props.location.pathname, '/resources/')
+
+    return (
+      <div>
+        <div
+          style={{
+            background: colors.bg,
+            color: colors.fg,
+            marginBottom: rhythm(1.5),
+          }}
+        >
+          <Container
+            style={{
+              maxWidth: 960,
+              padding: `${rhythm(1/2)}`,
+              paddingBottom: `${rhythm(1/2)}`,
+            }}
+          >
+            <Grid
+              columns={12}
+              style={{
+                padding: `${rhythm(1/2)} 0`,
+              }}
+            >
+              <Span
+                columns={4}
+                style={{
+                  height: 24, // Ugly hack. How better to constrain height of div?
+                }}
+              >
+                <Link
+                  to={prefixLink('/')}
+                  style={{
+                    textDecoration: 'none',
+                    color: colors.fg,
+                    fontSize: fontSizeToPx('25.5px').fontSize,
+                  }}
+                >
+                  {config.siteTitle}
+                </Link>
+              </Span>
+              <Span columns={8} last>
+                <a
+                  style={{
+                    float: 'right',
+                    color: colors.fg,
+                    textDecoration: 'none',
+                    marginLeft: rhythm(1/2),
+                  }}
+                  href="https://github.com/gatsbyjs/gatsby"
+                >
+                  Github
+                </a>
+                <Link
+                  to={prefixLink('/examples/')}
+                  style={{
+                    background: resourcesActive ? activeColors.bg : colors.bg,
+                    color: resourcesActive ? activeColors.fg : colors.fg,
+                    float: 'right',
+                    textDecoration: 'none',
+                    paddingLeft: rhythm(1/2),
+                    paddingRight: rhythm(1/2),
+                    paddingBottom: rhythm(1),
+                    marginBottom: rhythm(-1),
+                    paddingTop: rhythm(1),
+                    marginTop: rhythm(-1),
+                  }}
+                >
+                  Resources
+                </Link>
+                <Link
+                  to={prefixLink('/guide/')}
+                  style={{
+                    background: guideActive ? activeColors.bg : colors.bg,
+                    color: guideActive ? activeColors.fg : colors.fg,
+                    float: 'right',
+                    textDecoration: 'none',
+                    paddingLeft: rhythm(1/2),
+                    paddingRight: rhythm(1/2),
+                    paddingBottom: rhythm(1),
+                    marginBottom: rhythm(-1),
+                    paddingTop: rhythm(1),
+                    marginTop: rhythm(-1),
+                  }}
+                >
+                  Guide
+                </Link>
+              </Span>
+            </Grid>
+          </Container>
+        </div>
+        <Container
+          style={{
+            maxWidth: 720,
+            padding: `${rhythm(2)} ${rhythm(4)}`,
+          }}
+        >
+          {this.props.children}
+        </Container>
+      </div>
+    )
+  },
+})
